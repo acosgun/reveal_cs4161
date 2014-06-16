800dc0a5020e37d8b68db895ceffd20b53b3c49e
Akansel Cosgun <akansel.cosgun@gmail.com>
2014-06-16 06:36:15 -0400

    Added New Post Controller, minor edits.

---
+++ NewPostViewController.h
@@ -0,0 +1,14 @@
+//
+//  NewPostViewController.h
+//  Reveal
+//
+//  Created by Akansel Cosgun on 6/16/14.
+//  Copyright (c) 2014 Georgia Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewPostViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+@end
